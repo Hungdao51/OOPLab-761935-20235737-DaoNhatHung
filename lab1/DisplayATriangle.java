258f2a9a6536492c6e21dc8e54a3a944ce94f130
Đào Nhật Hưng <hung.dn235737@sis.hust.edu.vn>
2026-03-27 14:59:16 +0700

    Add files via upload

---
+++ lab1/DisplayATriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DisplayATriangle {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle: ");
+        int n = keyboard.nextInt();
+        keyboard.close();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
